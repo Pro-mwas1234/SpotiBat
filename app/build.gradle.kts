34b5b55ea2f36d03cc755f0494861b62d14d8ea2
pro-mwas1234 <shawnmwask1234@gmail.com>
2026-08-06 23:48:50 +0000

    Bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.project.lol"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.5"
+        versionCode = 5
+        versionName = "1.0.6"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
         }
