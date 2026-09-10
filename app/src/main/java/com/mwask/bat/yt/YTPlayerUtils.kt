5e655ffd483cac0d4ffef065040037f891d66639
pro-mwas1234 <shawnmwask1234@gmail.com>
2026-09-11 01:20:24 +0300

    Update GPL header attribution

--- app/src/main/java/com/mwask/bat/yt/YTPlayerUtils.kt
+++ app/src/main/java/com/mwask/bat/yt/YTPlayerUtils.kt
@@ -1,6 +1,6 @@
 /**
  * Metrolist Project (C) 2026
- * Licensed under GPL-3.0 | See git history for contributors
+ * Licensed under GPL-3.0 | Copyright (C) 2026 pro-mwas1234
  */
 
 package com.mwask.bat.yt
