5c7b686b8df763686301bdeca73e4969aa6ba7af
pro-mwas1234 <shawnmwask1234@gmail.com>
2026-09-16 00:56:16 +0300

    Bump to v1.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.mwask.bat"
         minSdk = 28
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.1.8"
+        versionCode = 20
+        versionName = "1.1.9"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
         }
